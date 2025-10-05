5aebce9e01dd4b6787b8a9c4fb8a2ccfb3ef4e5d
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-10-05 12:18:02 +0500

    task1

--- Tyuiu.GolovanovDO.Sprint2.Task1.V25.Lib/DataService.cs
+++ Tyuiu.GolovanovDO.Sprint2.Task1.V25.Lib/DataService.cs
@@ -15,12 +15,12 @@ namespace Tyuiu.GolovanovDO.Sprint2.Task1.V25.Lib
             
             bool[] res = new bool[6];
 
-            res[0] = c == (b * 2 + 15) | (a == c); //true
-            res[1] = (c != (b * 2 + 15)) & (a != d); //false
-            res[2] = (b < c) || (a < d);   //true
-            res[3] = (a > d) && (d > a);     //false
+            res[0] = c == (b * 2 + 15) | (a != c); //true
+            res[1] = (c != (b * 2 + 15)) & (a == d); //false
+            res[2] = (b < c) || (d > a);   //true
+            res[3] = (a > d) && (a < d);     //false
             res[4] = !(res[3]) ;//true
-            res[5] = (d >= a) ^ (c >= b); //false
+            res[5] = (d >= a) ^ (b <= c); //false
 
             return res;
         }

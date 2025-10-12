0e439c3681c5e76af1c50d87e10f029f5942c544
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-10-12 22:38:01 +0500

    task4

--- Tyuiu.GolovanovDO.Sprint2.Task4.V14.Lib/DataService.cs
+++ Tyuiu.GolovanovDO.Sprint2.Task4.V14.Lib/DataService.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.GolovanovDO.Sprint2.Task4.V14.Lib
         {
             double z = x * 3 < Math.Sqrt(y) + 20 
                 ? Math.Pow(2 + (1 / Math.Pow(x, 2)), y) 
-                : (Math.Pow(y, 2) - Math.Cos(Math.Pow(x, 2)) + 10) 
-                / (Math.Pow(x, 2) - Math.Sin(Math.Pow(y, 2) + 12));
+                : (Math.Pow(y, 2) - Math.Pow(Math.Cos(x), 2) + 10) 
+                / (Math.Pow(x, 2) - Math.Pow(Math.Sin(y), 2) + 12);
             return Math.Round(z, 3);
         }
     }
